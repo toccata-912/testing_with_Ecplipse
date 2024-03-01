8514edc0bf50bb48e2d5acfbc4783946d7c22ae6
toccata <130076100+toccata-912@users.noreply.github.com>
2024-03-01 18:47:38 +0100

    Update Main.java
    fixed space

--- src/pack/Main.java
+++ src/pack/Main.java
@@ -4,7 +4,6 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("first try with git and github");
 		System.out.println("now more");
-
 		System.out.println("even MOOOOOOOOOOOOOORE");
 	}
 }
