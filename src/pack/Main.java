500e4717e9161389d6d655913c95299cc684a7ab
Mohamed Derrahi <mohamedderrahi8@gmail.com>
2024-03-01 17:30:14 +0100

    Second take

--- src/pack/Main.java
+++ src/pack/Main.java
@@ -3,5 +3,7 @@ package pack;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("first try with git and github");
+		System.out.println("now more");
+		System.out.println("evern MORE");
 	}
 }
